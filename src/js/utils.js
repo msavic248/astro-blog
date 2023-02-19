6fe006e0cc08b6b7bad4d6135fb2440365ad9fa8
Milos Savic <msavic248@yahoo.com>
2023-02-19 12:54:37 +0100

    Added filter/sort to blog posts and updated home

--- src/js/utils.js
+++ src/js/utils.js
@@ -9,8 +9,45 @@ export function slugify(text) {
       .replace(/-+$/, '');
   }
   
-  export function formatDate(date) {
-    return new Date(date).toLocaleDateString('en-UK', {
-      timeZone: "GMT",
-    })
-  }
+export function formatDate(date) {
+	return new Date(date).toLocaleDateString('en-UK', {
+		timeZone: "GMT",
+	})
+}
+
+export function formatBlogPosts(posts, {
+	filterOutDrafts = true,
+	filterOutFuturePosts = true,
+	sortByDate = true,
+	limit = 99
+} = {}){
+
+	const filteredPosts = posts.reduce((acc, post) => {
+		const { date, draft } = post.frontmatter;
+		
+		//filterOutDrafts if ture
+		if(filterOutDrafts && draft) return acc;
+
+		//filterOutFuturePosts if true
+		if(filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+		//add post to acc
+		acc.push(post)
+
+		return acc;
+	}, [])
+
+	//sortByDate or randomize
+	if(sortByDate){
+		filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+	} else {
+		filteredPosts.sort(() => Math.random()-0.5)
+	}
+
+	//limit if number is passed
+	if(typeof limit === "number"){
+		return filteredPosts.slice(0, limit)
+	}
+
+	return filteredPosts;
+}
